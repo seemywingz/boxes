49207bcc9602a61c28a7101886603b46bb062a45
seemywings <kevin.jayne@icloud.com>
2024-07-13 17:43:00 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import * as tb from '3base';
 import Boxes from './Boxes';
 
-alert('Hello world!');
 let manager = new tb.Manager({
     "onLoad": () => {
         document.getElementById('loading-screen').classList.add('fade-out');
